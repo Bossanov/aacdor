41d2a82c9c44d43490b4345770956062250562af
Bossanov <31960270+Bossanov@users.noreply.github.com>
2021-12-23 10:52:44 +0100

    debug 3

--- config/initializers/search_flip.rb
+++ config/initializers/search_flip.rb
@@ -2,3 +2,6 @@
 #SearchFlip::Config[:base_url] = "http://localhost:9200"
 SearchFlip::Config[:environment] = "development"
 SearchFlip::Config[:base_url] = "http://127.0.0.1:9200"
+if Rails.env == 'production'
+  SearchFlip::Config[:base_url] = ENV["BONSAI_URL"]
+end
