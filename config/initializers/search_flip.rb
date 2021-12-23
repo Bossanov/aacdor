2e55aa7a110ee16fbd955c9a43acb2861c546632
Bossanov <31960270+Bossanov@users.noreply.github.com>
2021-12-23 09:27:32 +0100

    debug 2 elastic

--- config/initializers/search_flip.rb
+++ config/initializers/search_flip.rb
@@ -1,2 +1,4 @@
-SearchFlip::Config[:environment] = Rails.env
-SearchFlip::Config[:base_url] = "http://localhost:9200"
+#SearchFlip::Config[:environment] = Rails.env
+#SearchFlip::Config[:base_url] = "http://localhost:9200"
+SearchFlip::Config[:environment] = "development"
+SearchFlip::Config[:base_url] = "http://127.0.0.1:9200"
