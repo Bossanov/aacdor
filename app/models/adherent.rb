ccd0fe59503c269e497c1380df58d58cf03f264a
Bossanov <31960270+Bossanov@users.noreply.github.com>
2021-12-23 08:22:49 +0100

    debug elasticsearch

--- app/models/adherent.rb
+++ app/models/adherent.rb
@@ -3,4 +3,6 @@ class Adherent < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   has_many :etablissements
+  include SearchFlip::Model
+  notifies_index(AdherentIndex)
 end
